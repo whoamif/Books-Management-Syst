b371080be4ee4c0f1f5c22eaf38107b9b5666bda
whoamif <adjissifatimaamina@gmail.com>
2024-02-12 01:12:15 +0100

    updated

--- venv/database.py
+++ venv/database.py
@@ -11,5 +11,23 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = "sqlite:///./books.db"
 #url to the data base 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+#This line creates a database engine using the create_engine function.
+# It takes the database URL (SQLALCHEMY_DATABASE_URL) as its first argument 
+#and an optional connect_args parameter to specify additional connection arguments.
+# In this case, it sets check_same_thread to False, which is required when using SQLite
+# with multiple threads.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#This line creates a sessionmaker object named SessionLocal. It takes several parameters:
+
+#autocommit=False: This disables autocommit mode for sessions created by this sessionmaker.
+# In autocommit mode, each database operation is automatically committed.
+#autoflush=False: This disables autoflush mode for sessions created by this sessionmaker. 
+#Autoflush automatically flushes pending changes to the database before executing queries.
+#This binds the sessionmaker to the database engine created earlier (engine), 
+#so that sessions created by this sessionmaker will use that engine to connect 
+#to the database.
 Base = declarative_base()
+#This line creates a base class for declarative class definitions using 
+#the declarative_base 
+#function. The resulting Base object is used as a base class for all ORM class definitions.
+# It provides common functionality such as table creation and reflection.
